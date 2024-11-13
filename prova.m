9f5a01161db49f67b4f3de232b78f964e442d817
davideturrina <davide.turrina@live.it>
2024-11-13 02:23:06 +0100

    self control prova

---
+++ prova.m
@@ -0,0 +1,44 @@
+Oh, the night is my world
+City life, painted girls
+In a day, nothing matters
+It's the night time that flatters
+In the night, no control
+Through the wall, something breakin'
+Wearin' white as you're walkin'
+Down the street of my soul
+You take my self, you take my self control
+You got me livin' only for the night
+Before the morning comes the story's told
+You take my self you take my self control
+Another night, another day goes by
+I never stop myself to wonder why
+You're makin' me forget to play my role
+You take my self, you take my self control
+Oh, the night is my world
+City life, painted girls
+In a day, nothing matters
+It's the night time that flatters
+You take my self, you take my self control
+You got me livin' only for the night
+Before the morning comes the story's told
+You take my self you take my self control
+Another night, another day goes by
+I never stop myself to wonder why
+You're makin' me forget to play my role
+You take my self, you take my self control
+I, I live among the creatures of the night
+I haven't got the will to try and fight
+Against a new tomorrow
+So I guess I just believe it that this night will never go
+Night, I'm livin' in a moment of a dream
+I know that life is not as it would seem
+I must believe in something
+So I make myself believin' that this night will never go
+Oh, oh, oh
+Oh, oh, oh
+Oh, oh, oh
+Oh, oh, oh
+Oh, oh, oh
+Oh, oh, oh
+Oh, oh, oh
+Oh, oh, oh

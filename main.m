f3654d043bff0078b786cc1c92d22990327950d6
davideturrina <davide.turrina@live.it>
2024-12-02 00:54:01 +0100

    add solar wind data

--- main.m
+++ main.m
@@ -1,12 +1,16 @@
 clear
 
-
-wind_powercurve = xlsread('turbine_power_curve_5_MW.xlsx','Sheet1','B2:E32'); %Power in KW : second column
-
 % demand h - kWH 
 demand = 'demand_data_hxh_8784h.csv';
 demand = readtable(demand, 'PreserveVariableNames', true);
 demand = demand(1:8784, 1:2);
+
+
+wind_powercurve = xlsread('turbine_power_curve_5_MW.xlsx','Sheet1','B2:E32'); %Power in KW : second column
+
+
+
+
 
 
 

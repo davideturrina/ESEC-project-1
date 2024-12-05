6e36583d70b731bd3f4d026660bac9bc0d88aac4
davideturrina <davide.turrina@live.it>
2024-12-05 22:55:51 +0100

    single outputs ready (a check is needed)

--- main.m
+++ main.m
@@ -4,11 +4,12 @@ clc;
 
 %% DATA EXTRACTION
 
-demandxcapita = readtable('demand_data_hxh_8784h.csv', 'PreserveVariableNames', true);
-demandxcapita = demandxcapita(1:8784, 1:2);
 population = 2273800;
 
+demandxcapita = readtable('demand_data_hxh_8784h.csv', 'PreserveVariableNames', true);
+demandxcapita = demandxcapita(1:8784, 1:2);
 demand_total = array2table(demandxcapita{:,1:2}, 'VariableNames', {'Hour', 'TotalDemand'});
+
 demand_total.TotalDemand = demand_total.TotalDemand * population;
 
 
@@ -19,21 +20,23 @@ wind_powercurve = wind_powercurve(:, 1:2);
 
 % Solar and wind data for each hour in Orlando
 solar_wind_data = readtable('solar_and_wind_data_hxh.csv', 'PreserveVariableNames', true);
-solar_wind_data = solar_wind_data(1:8784, 1:7);
-
+solar_wind_data = double(table2array(solar_wind_data(1:8784, 1:7)));
+    
 % Solar efficiency and panel area
 solar_efficiency = 0.197*0.96; %cell+inverter
 panel_area = 2.80; %m2
+solar_data5 = solar_wind_data(:, 5) / 1000;                 % Solar irradiance was in Wh, now is in kWh
+
+solar_data5(solar_wind_data(:,6) > 80) = 0;                 %accounnting for shadows beyond 90-10 degrees
+
 
 % Extract numerical values for solar and wind data
-solar_data5 = double(solar_wind_data{:,5}) / 1000;  % Solar irradiance was in Wh now is in kWh
-wind_data7 = double(solar_wind_data{:,7});
+
+wind_data7 = solar_wind_data(:,7);
 
 %wind accounting for roughness
 h1 = 50; h2 = 140; z0 = 1.6;
 wind_data7 = wind_data7 .* (log(h2 / z0) / log(h1 / z0)); 
-
-
 
 
 %% Energy output single panel
@@ -41,19 +44,14 @@ solar_output_matrix = solar_data5 .* panel_area .* solar_efficiency;
 total_solar_output = sum(solar_output_matrix);
 disp(['Total energy for one PV (2.80m²) in a year: ', num2str(total_solar_output), ' kWh']);
 
-%%  energy output for one wind turbine
+%% Energy output single turbine (with interpolation )
+ wind_speeds = wind_powercurve(:,1); 
+ power_output = wind_powercurve(:,2); 
+ interpolated_power = interp1(wind_speeds, power_output, wind_data7, 'linear', 'extrap');
+ energy_per_hour = interpolated_power; 
+ one_turbine_total_energy = sum(energy_per_hour);
+ disp(['Total energy produce for one turbine (w\ interpol): ', num2str(one_turbine_total_energy), ' kWh']);
 
-
-energy_per_hour = zeros(length(wind_data7), 1); % Preallocate energy vector
-
-for i = 1:length(wind_data7)
-    [~, idx] = min(abs(wind_powercurve(:,1) - wind_data7(i))); %%AFONSO: i think this is the easiest method, but we can also interpolate the powercurve for a more precise data
-    power = wind_powercurve(idx, 2); % Correctly access the power value
-    energy_per_hour(i) = power * 1; % Power (kW) * Time (1 hour)
-end
-
-total_wind_output = sum(energy_per_hour);
-disp(['Total annual energy for one turbine: ', num2str(total_wind_output), ' kWh']);
 
 %% PLOTS
 
@@ -77,26 +75,19 @@ grid on;
 time = 1:height(solar_wind_data); 
 figure;
 yyaxis left;
-plot(time, solar_wind_data{:,5}, 'DisplayName', 'Solar Data', 'LineWidth', 1.5);
+plot(time, solar_wind_data(:,5), 'DisplayName', 'Solar Data', 'LineWidth', 1.5);
 ylabel('Solar Power (kW)');
-ylim([0 max(solar_wind_data{:,5})*1.1]); 
+ylim([0 max(solar_wind_data(:,5))*1.1]); 
 yyaxis right;
-plot(time, solar_wind_data{:,7}, 'DisplayName', 'Wind Data', 'LineWidth', 1.5);
+plot(time, solar_wind_data(:,7), 'DisplayName', 'Wind Data', 'LineWidth', 1.5);
 ylabel('Wind Power (kW)');
-ylim([0 max(solar_wind_data{:,7})*1.1]); 
+ylim([0 max(solar_wind_data(:,7))*1.1]); 
 xlabel('Time (hours)');
 title('Solar and Wind Power Data');
 legend('Solar Power', 'Wind Power');
 grid on;
 
 
-%% W\ INTERPOL for wind powcurve (output slightly different)
- wind_speeds = wind_powercurve(:,1); 
- power_output = wind_powercurve(:,2); 
- interpolated_power = interp1(wind_speeds, power_output, wind_data7, 'linear', 'extrap');
- energy_per_hour = interpolated_power; 
- one_turbine_total_energy = sum(energy_per_hour);
- disp(['Energia totale prodotta in un anno con interpolazione: ', num2str(one_turbine_total_energy), ' kWh']);
 
 
 
